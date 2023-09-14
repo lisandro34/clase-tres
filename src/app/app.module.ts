3f680d38547d1691e69f1e90053105bca66bf7eb
lisandro34 <reinozolisandro@gmail.com>
2023-09-13 22:51:25 -0300

    se agrega la clase-tres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { ListaComponent } from './lista/lista.component';
+import { ContadorComponent } from './contador/contador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    ListaComponent,
+    ContadorComponent
   ],
   imports: [
     BrowserModule
